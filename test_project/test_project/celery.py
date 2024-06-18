a7abeff3cffe65e12aab5a95b058858956a765dd
AlenaSapfirova <121548066+AlenaSapfirova@users.noreply.github.com>
2024-06-18 09:41:29 +0300

    Update celery.py

--- test_project/test_project/celery.py
+++ test_project/test_project/celery.py
@@ -11,13 +11,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.task_track_started = True
 app.conf.task_ignore_result = False
 app.autodiscover_tasks()
-# import os
-# from celery import Celery
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
-# app = Celery('test_project', broker_connection_retry=False,
-#              broker_connection_retry_on_startup=True, )
-# app.config_from_object('django.conf:settings')
-# broker_connection_retry = False
-
-# app.autodiscover_tasks()
